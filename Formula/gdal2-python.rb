fe97443221bee8b4aba97aa2cbbd447e47bed110
kkdd <kkondo_z0@nifty.com>
2018-02-17 23:42:29 +0900

    gdal2-python: creating gdal2-python.pth
    This creates site-packages/gdal2-python.pth.

--- Formula/gdal2-python.rb
+++ Formula/gdal2-python.rb
@@ -89,8 +89,10 @@ class Gdal2Python < Formula
 
       Language::Python.each_python(build) do |python, _python_version|
         system python, *Language::Python.setup_install_args(prefix)
+        system "echo", "#{opt}/#{name}/lib/python#{_python_version}/site-packages",
+                ">", "#{lib}/python#{_python_version}/site-packages/#{name}.pth"
       end
-
+    
       # Scripts compatible with Python3? Appear to be...
       bin.install Dir["scripts/*"]
       # Clean up any stray doxygen files.
